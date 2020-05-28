9b9efed47133f9a436caf9e9e507c9e7c06e8d48
hudaokeji <github@hudaokeji.com>
2020-05-28 13:47:04 +0900

    add original version

---
+++ .original/jsbin-4.1.7/lib/processors/markdown.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var path = require('path');
+var root = path.resolve(path.join(__dirname, '../../'));
+var marked = require(root + '/public/js/vendor/marked.min.js');
+
+module.exports = function (data) {
+  return new Promise(function (resolve, reject) {
+    try {
+      var res = marked(data.source);
+      resolve({
+        errors: null,
+        result: res
+      });
+    } catch (e) {
+      var errors = {
+        line: null,
+        ch: null,
+        msg: e
+      };
+      resolve({
+        errors: [errors],
+        result: null
+      });
+    }
+  });
+};
